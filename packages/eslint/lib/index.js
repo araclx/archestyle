b3b41ec62f2c5253129a328132ecaaeab534be7e
Jakub Olan <jakub.olan001@gmail.com>
2020-07-31 07:53:44 +0200

    feat(eslint): added basic configuration

--- packages/eslint/lib/index.js
+++ packages/eslint/lib/index.js
@@ -1,7 +1,24 @@
-'use strict';
+"use strict";
 
 module.exports = eslint;
 
-function eslint() {
-    // TODO
-}
+// TODO: React.js Support
+
+const eslint = {
+  extends: [
+    './rules/best-practices',
+    './rules/errors',
+    './rules/es6',
+    './rules/imports',
+    './rules/node',
+    './rules/strict',
+    './rules/style',
+    './rules/variables',
+    './rules/typescript'
+  ]
+  parser: "@typescript-eslint/parser",
+  parserOptions: { sourceType: "module" }
+  plugins: [
+    '@typescript-eslint'
+  ]
+};
